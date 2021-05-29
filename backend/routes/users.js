74ce07ff42407f8d5913cd97b994c25726e237cf
Andres Rosales <andresrosalespinzon@gmail.com>
2021-05-29 16:34:46 +0200

    feat: added lucnhweek route with dummy data with dinamic routing

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router()
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-  res.send('respond with a resource')
+  const user = {
+    id: 1,
+    name: 'John Doe',
+    emailAddress: 'johnny@test.com',
+  }
+  res.send(user)
 })
 
 module.exports = router
