e7f92180fc7e09ef2c767d4d784e5c0a818de8f8
Andres Rosales <andresrosalespinzon@gmail.com>
2021-06-04 14:41:13 +0200

    feat: auth with auth0

--- frontend/src/views/admin/AdminLayout.svelte
+++ frontend/src/views/admin/AdminLayout.svelte
@@ -2,27 +2,43 @@
   import { Route } from 'svelte-router-spa'
   import { onMount } from 'svelte'
   import { user } from '../../store/stores'
+  import createAuth0Client from '@auth0/auth0-spa-js'
+  import auth0Config from '../../auth0-config'
 
   export let currentRoute
-
   let initialized = false
 
   // lifecycle function
-  onMount(() => {
-    // stores are written to be calling set
-    user.set({
-      name: 'Test user',
+  onMount(async () => {
+    let auth0 = await createAuth0Client(auth0Config)
+    const authenticated = await auth0.isAuthenticated()
 
-      
-      schoolName: 'Test school',
-    })
-    initialized = true
+    if (!authenticated) {
+      await auth0.loginWithRedirect({
+        redirect_uri: `${window.location.origin}/callback`,
+        appState: window.location.pathname,
+      })
+    } else {
+      user.set({
+        name: 'Test User',
+        schoolName: 'Test School',
+      })
+      initialized = true
+    }
   })
+
+  const logout = async () => {
+    // todo
+  }
 
 </script>
 
 <div>
   {#if initialized}
+    <button
+      class="button is-light is-small"
+      style="float: right;"
+      on:click="{() => logout()}">Logout</button>
     <Route.default currentRoute="{currentRoute}" />
   {/if}
 </div>
